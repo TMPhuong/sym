24a35a4f57a1afb56cfff8de4a9ef73562540019
Tran Phuong <tmphuong90@gmail.com>
2016-08-07 17:15:16 +0800

    Initial setup

---
+++ src/main/java/Application.java
@@ -0,0 +1,25 @@
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@Slf4j
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        log.info("application is started");
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+
+}
---
+++ src/main/java/MyRestController.java
@@ -0,0 +1,12 @@
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
