59b13e616a8be8a4c8a1b71f2ee3e8a128bc8bed
Tran Phuong <tmphuong90@gmail.com>
2016-09-12 16:50:04 +0800

    Minor changes

--- src/main/java/handler/PriceDataGenerator.java
+++ src/main/java/handler/PriceDataGenerator.java
@@ -2,10 +2,9 @@ package handler;
 
 import model.CurrencyPairConfig;
 import model.PriceData;
+import model.PriceSide;
 import org.apache.commons.lang3.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.concurrent.ScheduledExecutorTask;
 import reactor.bus.Event;
 import reactor.bus.EventBus;
 
@@ -13,7 +12,6 @@ import java.math.BigDecimal;
 import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 public class PriceDataGenerator {
@@ -35,16 +33,24 @@ public class PriceDataGenerator {
             CurrencyPairConfig currencyPairConfig = entry.getValue();
             String currencyPair = entry.getKey();
             executorService.scheduleWithFixedDelay(() -> {
-                BigDecimal bidPrice = generateFakePrice(currencyPairConfig.getMinBidPrice(), currencyPairConfig.getMaxBidPrice());
-                BigDecimal askPrice = generateFakePrice(currencyPairConfig.getMinAskPrice(), currencyPairConfig.getMaxAskPrice());
+                BigDecimal bidPrice = generateFakePrice(currencyPairConfig.getMinBidPrice(), currencyPairConfig.getMaxBidPrice(), currencyPairConfig.getPricePrecision(), PriceSide.BUY);
+                BigDecimal askPrice = generateFakePrice(currencyPairConfig.getMinAskPrice(), currencyPairConfig.getMaxAskPrice(), currencyPairConfig.getPricePrecision(), PriceSide.SELL);
                 eventBus.notify("new_price", Event.wrap(new PriceData(currencyPair, "SP", bidPrice, askPrice)));
             }, priceGeneratorInitialDelay, currencyPairConfig.getFixedDelay(), TimeUnit.MILLISECONDS);
         }
 
     }
 
-    private BigDecimal generateFakePrice(BigDecimal min, BigDecimal max, Byte precision) {
-        RandomUtils.
-        return null;
+    private BigDecimal generateFakePrice(BigDecimal min, BigDecimal max, Byte precision, PriceSide side) {
+        double randomPrice = RandomUtils.nextDouble(min.doubleValue(), max.doubleValue());
+        return BigDecimal.valueOf(randomPrice).setScale(precision, getRoundingMode(side));
+    }
+
+    private int getRoundingMode(PriceSide side) {
+        switch (side) {
+            case BUY: return BigDecimal.ROUND_FLOOR;
+            case SELL: return BigDecimal.ROUND_CEILING;
+            default: return BigDecimal.ROUND_HALF_UP;
+        }
     }
 }
